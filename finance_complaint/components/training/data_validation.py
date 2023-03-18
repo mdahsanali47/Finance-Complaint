22f60040aadfbf481e32de837dbc8c7eecb49a4c
Md Ahsan Ali <mdahsanali47@gmail.com>
2023-03-18 20:03:52 +0530

    updated data-ingestion

--- finance_complaint/components/training/data_validation.py
+++ finance_complaint/components/training/data_validation.py
@@ -42,7 +42,7 @@ class DataValidation(FinanceDataSchema):
         try:
             dataframe: DataFrame = spark_session.read.parquet(
                 self.data_ingestion_artifact.feature_store_file_path
-            ).limit(10000)
+            )
             logging.info(f"Data frame is created using file: {self.data_ingestion_artifact.feature_store_file_path}")
             logging.info(f"Number of row: {dataframe.count()} and column: {len(dataframe.columns)}")
             #dataframe, _ = dataframe.randomSplit([0.001, 0.999])
